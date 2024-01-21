5f6bd3b9d3adaf329cda50f459368fedd4a7d6ce
Tadeusz Kamyszek <tkamyszek@op.pl>
2024-01-21 23:25:32 +0100

    ADD: BaseEmployee

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,10 @@ import java.io.PrintStream;
 
 public class Main {
 
+
     public static void main(String[] args) {
+
+    /*
     Seat seat = new Seat(1,10);
     System.out.println(seat.seatStatus());
     Seat seatTwo = new Seat(1,10, true);
@@ -14,6 +17,23 @@ public class Main {
 
     Movie movieNew = new Movie("The Green Mile", "Frank", "Darabont", 181, 1999);
     movieNew.movieInfo();
+    */
 
-}
+    TicketSeller employee = new TicketSeller("Tadeusz", "Kowalski",1993);
+    Manager manager = new Manager("Szymon", "Kowalski",2000);
+
+    System.out.println("Employee: ");
+    System.out.println("Full name: " + employee.getFullName());
+    System.out.println("Years of employment: " + employee.getYearsOfEmployment());
+    System.out.println("Salary: " + employee.calculateMonthlySalary());
+
+    System.out.println("Manager: ");
+    System.out.println("Full name: " + manager.getFullName());
+    System.out.println("Years of employment: " + manager.getYearsOfEmployment());
+    System.out.println("Salary: " + manager.calculateMonthlySalary());
+
+    manager.setBonus(800);
+    System.out.println("Manager salary + new bonus: " + manager.calculateMonthlySalary());
+
+    }
 }
