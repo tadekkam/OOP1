490e6834442f0dde0540086611d9283d221f0984
Tadeusz Kamyszek <tkamyszek@op.pl>
2024-01-23 20:08:38 +0100

    ADD: Pythagoras, Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,44 +7,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-    /*
-    Seat seat = new Seat(1,10);
-    System.out.println(seat.seatStatus());
-    Seat seatTwo = new Seat(1,10, true);
-    System.out.println(seatTwo.seatStatus());
-    seat.isOccupied = true;
-    System.out.println(seat.seatStatus());
 
-    Movie movieNew = new Movie("The Green Mile", "Frank", "Darabont", 181, 1999);
-    movieNew.movieInfo();
-    */
-    /*
-    TicketSeller employee = new TicketSeller("Tadeusz", "Kowalski",1993);
-    Manager manager = new Manager("Szymon", "Kowalski",2000);
+        boolean pythagoras = Pythagoras.isRightTriangleExc(3,4,5);
+        System.out.println(pythagoras);
 
-    System.out.println("Employee: ");
-    System.out.println("Full name: " + employee.getFullName());
-    System.out.println("Years of employment: " + employee.getYearsOfEmployment());
-    System.out.println("Salary: " + employee.calculateMonthlySalary());
-
-    System.out.println("Manager: ");
-    System.out.println("Full name: " + manager.getFullName());
-    System.out.println("Years of employment: " + manager.getYearsOfEmployment());
-    System.out.println("Salary: " + manager.calculateMonthlySalary());
-
-    manager.setBonus(800);
-    System.out.println("Manager salary + new bonus: " + manager.calculateMonthlySalary());
-
-
-        int minutes = convert(10);
-        System.out.println(minutes);
-    */
-
-        boolean pitagoras = Pitagoras.isRightTriangle(3,4,5);
-        System.out.println(pitagoras);
-
-        boolean pitagorasError = Pitagoras.isRightTriangle(0,4,5);
-        System.out.println(pitagorasError);
+        boolean pythagorasError = Pythagoras.isRightTriangleExc(0,4,5);
+        System.out.println(pythagorasError);
 
     }
 }
